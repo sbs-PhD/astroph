b47b9884da7d8953f662c5d60925e264fb574a07
sbs-PhD <47783932+sbs-PhD@users.noreply.github.com>
2019-02-20 17:26:27 -0300

    Add files via upload

---
+++ accuracyOV236080.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+## %matplotlib inline
+import os
+import matplotlib.pyplot as plt
+import numpy as np, pandas as pd
+from sklearn import metrics, model_selection
+import xgboost as xgb
+from xgboost.sklearn import XGBClassifier
+
+data = pd.read_csv('input/ov236-080-mjd.csv', names=['mjd','date','S','sigS'])
+data.head()
+data.info()
+
+data['mjd'],class_names = pd.factorize(data['mjd'])
+print(class_names)
+print(data['mjd'].unique())
+
+data['date'],_ = pd.factorize(data['date'])
+data['S'],_ = pd.factorize(data['S'])
+data['sigS'],_ = pd.factorize(data['sigS'])
+
+X = data.iloc[:,:-1]
+y = data.iloc[:,-1]
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3, random_state=123)
+
+params = {
+    'objective': 'binary:logistic',
+    'max_depth': 4,
+    'learning_rate': 1.0,
+    'silent': 1.0,
+    'n_estimators': 5
+}
+
+
+model = XGBClassifier(**params).fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+count_misclassified = (y_test != y_pred).sum()
+print('Misclassified samples: {}'.format(count_misclassified))
+accuracy = metrics.accuracy_score(y_test, y_pred)
+print('Accuracy: {:.2f}'.format(accuracy))
+
+classifier = xgb.sklearn.XGBClassifier(nthread=-1, seed=42)
+classifier.fit(X_train, y_train)
+
+plt.figure(figsize=(20,15))
+xgb.plot_importance(classifier, ax=plt.gca())
+plt.show()
+
+plt.figure(figsize=(20,15))
+xgb.plot_tree(classifier, ax=plt.gca())
+plt.show()
+
+print("Number of boosting trees: {}".format(classifier.n_estimators))
+print("Max depth of trees: {}".format(classifier.max_depth))
+print("Objective function: {}".format(classifier.objective))
---
+++ best_learning_rate_outburst_OV236048.py
@@ -0,0 +1,34 @@
+# Tune learning_rate
+from numpy import loadtxt
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+
+# load data
+dataset = loadtxt('input/ov236-048-mjd-new.csv', delimiter=",")
+
+# split data into X and y
+ncol = 4
+# X = dataset[:,0:ncol]
+# Y = dataset[:,ncol]
+
+print(X)
+print(Y)
+
+# grid search
+model = XGBClassifier()
+learning_rate = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3]
+param_grid = dict(learning_rate=learning_rate)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X, Y)
+
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
